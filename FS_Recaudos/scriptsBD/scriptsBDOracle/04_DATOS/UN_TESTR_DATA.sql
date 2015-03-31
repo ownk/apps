65254787831345dbc8bce5ea4b516836ac6092bc
Juan Camilo Vargas Jerez <juan.jerez@ownk.co>
2015-03-31 14:54:07 -0500

    Se ajustan scripts de base de datos

--- FS_Recaudos/scriptsBD/scriptsBDOracle/04_DATOS/UN_TESTR_DATA.sql
+++ FS_Recaudos/scriptsBD/scriptsBDOracle/04_DATOS/UN_TESTR_DATA.sql
@@ -1,12 +1,12 @@
 PROMPT INSERTANDO DATOS EN FS_RECAUDOS_US.UN_TESTR
 
-insert into UN_TESTR (ESTR_ESTR, ESTR_DESCRI, ESTR_USUA)
+insert into FS_RECAUDOS_US.UN_TESTR (ESTR_ESTR, ESTR_DESCRI, ESTR_USUA)
 values ('1', 'Estructura de archivo tipo FIDUCIARIA', 'operador');
 
-insert into UN_TESTR (ESTR_ESTR, ESTR_DESCRI, ESTR_USUA)
+insert into FS_RECAUDOS_US.UN_TESTR (ESTR_ESTR, ESTR_DESCRI, ESTR_USUA)
 values ('2', 'Estructura de archivo tipo ASOBANCARIA', 'operador');
 
-insert into UN_TESTR (ESTR_ESTR, ESTR_DESCRI, ESTR_USUA)
+insert into FS_RECAUDOS_US.UN_TESTR (ESTR_ESTR, ESTR_DESCRI, ESTR_USUA)
 values ('3', 'Estructura de archivo DEFAULT', 'operador');
 
 
