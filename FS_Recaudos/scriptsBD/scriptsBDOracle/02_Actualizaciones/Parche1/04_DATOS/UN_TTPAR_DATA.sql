f019216ca9b5cf8549a19e7619a6c46402882e4c
Juan Camilo Vargas Jerez <juan.jerez@ownk.co>
2015-04-24 02:46:17 -0500

    Se ajustan scripts bd parche1

---
+++ FS_Recaudos/scriptsBD/scriptsBDOracle/02_Actualizaciones/Parche1/04_DATOS/UN_TTPAR_DATA.sql
@@ -0,0 +1,31 @@
+PROMPT ACTUALIZANDO DATOS EN FS_RECAUDOS_US.FS_RECAUDOS_US.un_ttpar
+
+update FS_RECAUDOS_US.un_ttpar set tpar_nomb_arun = 'Cta_2242'
+where TPAR_TPAR = 'D42';
+
+update FS_RECAUDOS_US.un_ttpar set tpar_nomb_arun = 'Cta_8745'
+where TPAR_TPAR = 'D45';
+
+update FS_RECAUDOS_US.un_ttpar set tpar_nomb_arun = 'Cta_4366'
+where TPAR_TPAR = 'D66';
+
+update FS_RECAUDOS_US.un_ttpar set tpar_nomb_arun = 'Cta_7893'
+where TPAR_TPAR = 'D93';
+
+update FS_RECAUDOS_US.un_ttpar set tpar_nomb_arun = 'Cta_4125'
+where TPAR_TPAR = 'D25';
+
+update FS_RECAUDOS_US.un_ttpar set tpar_nomb_arun = 'Cta_3403'
+where TPAR_TPAR = 'D03';
+
+update FS_RECAUDOS_US.un_ttpar set tpar_nomb_arun = 'Cta_3410'
+where TPAR_TPAR = 'D10';
+
+update FS_RECAUDOS_US.un_ttpar set tpar_nomb_arun = 'Cta_3650'
+where TPAR_TPAR = 'D50';
+
+update FS_RECAUDOS_US.un_ttpar set tpar_nomb_arun = 'FIDUUNIVA'
+where TPAR_TPAR = 'TXT';
+
+
+commit;
