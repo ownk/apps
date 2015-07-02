37fd53ea226671d6817b1279b9355a4174ef74bc
Juan Camilo Vargas Jerez <juan.jerez@ownk.co>
2015-07-02 00:32:21 -0500

    Se crea scripta para insertar estados del archivo generado

---
+++ FS_Recaudos/scriptsBD/scriptsBDOracle/02_ConvertidorArchivos/01_lineaBase/04_DATOS/CO_TEARGE_DATA.sql
@@ -0,0 +1,8 @@
+PROMPT INSERTANDO DATOS EN FS_RECAUDOS_US.CO_TEARGE
+
+Insert into FS_RECAUDOS_US.CO_TEARGE (EARGE_EARGE,EARGE_DESCRI) values ('GENERADO','Archivo creado luego de la conversion');
+
+
+
+
+commit;
